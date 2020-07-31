299f689ab258d03d113898f537c3d1f0bec6dbf4
aravindsp <pillai.aravinds@gmail.com>
2020-07-31 11:38:04 -0700

    tst

--- sp copy.js
+++ sp copy.js
@@ -4,62 +4,6 @@ var margin = {top: 10, right: 30, bottom: 30, left: 60},
     height = 400 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
-var startDate = new Date("2004-11-01"),
-    endDate = new Date("2017-04-01");
-
-    var svgSlider = d3.select("#slider")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height);
-    
-var x = d3.scaleTime()
-    .domain([startDate, endDate])
-    .range([0, width])
-    .clamp(true);
-
-var slider = svgSlider.append("g")
-    .attr("class", "slider")
-    .attr("transform", "translate(" + margin.left + "," + height / 2 + ")");
-
-slider.append("line")
-    .attr("class", "track")
-    .attr("x1", x.range()[0])
-    .attr("x2", x.range()[1])
-  .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
-    .attr("class", "track-inset")
-  .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
-    .attr("class", "track-overlay")
-    .call(d3.drag()
-        .on("start.interrupt", function() { slider.interrupt(); })
-        .on("start drag", function() { update(x.invert(d3.event.x)); }));
-
-slider.insert("g", ".track-overlay")
-    .attr("class", "ticks")
-    .attr("transform", "translate(0," + 18 + ")")
-  .selectAll("text")
-    .data(x.ticks(10))
-    .enter()
-    .append("text")
-    .attr("x", x)
-    .attr("y", 10)
-    .attr("text-anchor", "middle")
-    .text(function(d) { return formatDateIntoYear(d); });
-
-var handle = slider.insert("circle", ".track-overlay")
-    .attr("class", "handle")
-    .attr("r", 9);
-
-var label = slider.append("text")  
-    .attr("class", "label")
-    .attr("text-anchor", "middle")
-    .text(formatDate(startDate))
-    .attr("transform", "translate(0," + (-25) + ")")
-});
-
-//////////////////////////////
-
-
-
 var svg = d3.select("#sp")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
